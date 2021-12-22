5c0a408130dc38e31d529ab7c46c1441600aebd5
vova203 <3615585@mail.ru>
2021-12-22 19:40:27 +0300

    10 задача дз 1

---
+++ Project1/Project(10)/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <stdlib.h>;
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int max = 0;
+	int min = 0;
+	cin >> a >> b;
+	max = (a + b + abs(a - b)) / 2;
+	min = (a + b - abs(a - b)) / 2;
+	cout << max << max << endl;
+
+	return EXIT_SUCCESS;
+}
